7f9ffd6f9e73ad0ab4faa084a3ffcc1ca086c5ff
windu2b <windu.2b@gmail.com>
2013-06-10 00:08:13 +0200

    réutilisation des valeurs envoyées par le formulaire

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ require_once( './config.inc.php' );
 
 if( isset( $_GET['submit'] ) )
 {
-	
 }
 ?>
 <html>
@@ -11,11 +10,11 @@ if( isset( $_GET['submit'] ) )
 	</head>
 	<body>
 		<form method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-			<input type="text" name="id" />
+			<input type="text" name="id" value="<?php echo $_GET['id']; ?>" />
 			<select name="primitives">
-				<option value="node">node</option>
-				<option value="way">way</option>
-				<option value="relation">relation</option>
+				<option value="node" <?php ( $_GET['primitives'] == 'node' ? 'selected="selected"' : '' ); ?>>node</option>
+				<option value="way" <?php ( $_GET['primitives'] == 'node' ? 'selected="selected"' : '' ); ?>>way</option>
+				<option value="relation" <?php ( $_GET['primitives'] == 'node' ? 'selected="selected"' : '' ); ?>>relation</option>
 			</select>
 			<input type="submit" name="submit" value="Valider" />
 		</form>
